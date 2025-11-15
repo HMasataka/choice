0ba3fbb099a3c5e804fb62b86a0925cbbb0d1b27
HMasataka <masataka.hisasue@gmail.com>
2025-11-15 21:36:27 +0900

    ntp.goに切り出し

--- pkg/sfu/utils.go
+++ pkg/sfu/utils.go
@@ -4,32 +4,10 @@ import (
 	"encoding/binary"
 	"strings"
 	"sync/atomic"
-	"time"
 
 	"github.com/HMasataka/choice/pkg/buffer"
 	"github.com/pion/webrtc/v4"
 )
-
-var ntpEpoch = time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC)
-
-func fastForwardTimestampAmount(newestTimestamp uint32, referenceTimestamp uint32) uint32 {
-	if buffer.IsTimestampWrapAround(newestTimestamp, referenceTimestamp) {
-		return uint32(uint64(newestTimestamp) + 0x100000000 - uint64(referenceTimestamp))
-	}
-
-	if newestTimestamp < referenceTimestamp {
-		return 0
-	}
-
-	return newestTimestamp - referenceTimestamp
-}
-
-func ntpToMillisSinceEpoch(ntp uint64) uint64 {
-	// ntp time since epoch calculate fractional ntp as milliseconds
-	// (lower 32 bits stored as 1/2^32 seconds) and add
-	// ntp seconds (stored in higher 32 bits) as milliseconds
-	return (((ntp & 0xFFFFFFFF) * 1000) >> 32) + ((ntp >> 32) * 1000)
-}
 
 func setVP8TemporalLayer(p *buffer.ExtPacket, d *downTrack) (buf []byte, picID uint16, tlz0Idx uint8, drop bool) {
 	pkt, ok := p.Payload.(buffer.VP8)
@@ -66,6 +44,7 @@ func setVP8TemporalLayer(p *buffer.ExtPacket, d *downTrack) (buf []byte, picID u
 
 	return
 }
+
 func modifyVP8TemporalPayload(payload []byte, picIDIdx, tlz0Idx int, picID uint16, tlz0ID uint8, mBit bool) {
 	pid := make([]byte, 2)
 	binary.BigEndian.PutUint16(pid, picID)
@@ -96,32 +75,4 @@ func codecParametersFuzzySearch(needle webrtc.RTPCodecParameters, haystack []web
 	}
 
 	return webrtc.RTPCodecParameters{}, webrtc.ErrCodecNotFound
-}
-
-type ntpTime uint64
-
-func (t ntpTime) Duration() time.Duration {
-	sec := (t >> 32) * 1e9
-	frac := (t & 0xffffffff) * 1e9
-	nsec := frac >> 32
-	if uint32(frac) >= 0x80000000 {
-		nsec++
-	}
-	return time.Duration(sec + nsec)
-}
-
-func (t ntpTime) Time() time.Time {
-	return ntpEpoch.Add(t.Duration())
-}
-
-func toNtpTime(t time.Time) ntpTime {
-	nsec := uint64(t.Sub(ntpEpoch))
-	sec := nsec / 1e9
-	nsec = (nsec - sec*1e9) << 32
-	frac := nsec / 1e9
-	if nsec%1e9 >= 1e9/2 {
-		frac++
-	}
-
-	return ntpTime(sec<<32 | frac)
 }
