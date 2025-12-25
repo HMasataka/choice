4cc4924687e3bdced120775b72e5e1a73a2f28ee
HMasataka <masataka.hisasue@gmail.com>
2025-12-25 19:45:29 +0900

    logの出力方法調整

--- _example/client/src/app/logger.ts
+++ _example/client/src/app/logger.ts
@@ -1,9 +1,3 @@
-import { elements } from "@ui/dom";
-
 export function log(...args: any[]) {
-  const line = args
-    .map((a) => (typeof a === "string" ? a : JSON.stringify(a)))
-    .join(" ");
-  elements.log.textContent += line + "\n";
-  elements.log.scrollTop = elements.log.scrollHeight;
+  console.log(...args);
 }
