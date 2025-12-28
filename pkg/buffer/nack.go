803806cbfecc0d5460329ab466a3092194aa8b42
HMasataka <masataka.hisasue@gmail.com>
2025-12-28 13:21:14 +0900

    pkg/buffer/nack.go のリファクタ

--- pkg/buffer/nack.go
+++ pkg/buffer/nack.go
@@ -26,41 +26,44 @@ func newNACKQueue() *nackQueue {
 	}
 }
 
-func (n *nackQueue) remove(extSN uint32) {
-	i := sort.Search(len(n.nacks),
-		func(i int) bool {
-			return n.nacks[i].sn >= extSN
-		},
-	)
+func (n *nackQueue) find(extSN uint32) int {
+	return sort.Search(len(n.nacks), func(i int) bool {
+		return n.nacks[i].sn >= extSN
+	})
+}
 
+func (n *nackQueue) remove(extSN uint32) {
+	i := n.find(extSN)
 	if i >= len(n.nacks) || n.nacks[i].sn != extSN {
 		return
 	}
-
 	copy(n.nacks[i:], n.nacks[i+1:])
-
 	n.nacks = n.nacks[:len(n.nacks)-1]
 }
 
 func (n *nackQueue) push(extSN uint32) {
-	i := sort.Search(len(n.nacks), func(i int) bool { return n.nacks[i].sn >= extSN })
+	i := n.find(extSN)
 	if i < len(n.nacks) && n.nacks[i].sn == extSN {
 		return
 	}
+	n.insertAt(i, extSN)
+	n.trimIfFull()
+}
 
-	nck := nack{
-		sn:     extSN,
-		nacked: 0,
-	}
+func (n *nackQueue) insertAt(i int, extSN uint32) {
+	nck := nack{sn: extSN, nacked: 0}
 	if i == len(n.nacks) {
 		n.nacks = append(n.nacks, nck)
 	} else {
 		n.nacks = append(n.nacks[:i+1], n.nacks[i:]...)
 		n.nacks[i] = nck
 	}
+}
 
+func (n *nackQueue) trimIfFull() {
 	if len(n.nacks) >= maxNackCache {
 		copy(n.nacks, n.nacks[1:])
+		n.nacks = n.nacks[:len(n.nacks)-1]
 	}
 }
 
@@ -69,41 +72,79 @@ func (n *nackQueue) pairs(headSN uint32) ([]rtcp.NackPair, bool) {
 	if len(n.nacks) == 0 {
 		return nil, false
 	}
-	i := 0
+
+	b := &nackPairBuilder{}
+	writeIdx := 0
 	askKF := false
-	var np rtcp.NackPair
-	var nps []rtcp.NackPair
+
 	for _, nck := range n.nacks {
-		if nck.nacked >= maxNackTimes {
-			if nck.sn > n.kfSN {
-				n.kfSN = nck.sn
-				askKF = true
-			}
-			continue
+		switch n.classifyNack(nck, headSN) {
+		case nackExpired:
+			askKF = n.checkKeyframeRequest(nck) || askKF
+		case nackTooRecent:
+			n.nacks[writeIdx] = nck
+			writeIdx++
+		case nackPending:
+			n.nacks[writeIdx] = nack{sn: nck.sn, nacked: nck.nacked + 1}
+			writeIdx++
+			b.add(nck.sn)
 		}
-		if nck.sn >= headSN-2 {
-			n.nacks[i] = nck
-			i++
-			continue
-		}
-		n.nacks[i] = nack{
-			sn:     nck.sn,
-			nacked: nck.nacked + 1,
-		}
-		i++
-		if np.PacketID == 0 || uint16(nck.sn) > np.PacketID+16 {
-			if np.PacketID != 0 {
-				nps = append(nps, np)
-			}
-			np.PacketID = uint16(nck.sn)
-			np.LostPackets = 0
-			continue
-		}
-		np.LostPackets |= 1 << (uint16(nck.sn) - np.PacketID - 1)
 	}
-	if np.PacketID != 0 {
-		nps = append(nps, np)
+
+	n.nacks = n.nacks[:writeIdx]
+	return b.build(), askKF
+}
+
+type nackStatus int
+
+const (
+	nackExpired   nackStatus = iota // NACK回数上限到達
+	nackTooRecent                   // 最近のパケット（まだNACKしない）
+	nackPending                     // NACK対象
+)
+
+func (n *nackQueue) classifyNack(nck nack, headSN uint32) nackStatus {
+	if nck.nacked >= maxNackTimes {
+		return nackExpired
 	}
-	n.nacks = n.nacks[:i]
-	return nps, askKF
+	if nck.sn >= headSN-2 {
+		return nackTooRecent
+	}
+	return nackPending
+}
+
+func (n *nackQueue) checkKeyframeRequest(nck nack) bool {
+	if nck.sn > n.kfSN {
+		n.kfSN = nck.sn
+		return true
+	}
+	return false
+}
+
+// nackPairBuilder はNACKペアのビットマップを構築する
+type nackPairBuilder struct {
+	pairs   []rtcp.NackPair
+	current rtcp.NackPair
+}
+
+func (b *nackPairBuilder) add(sn uint32) {
+	snU16 := uint16(sn)
+	if b.current.PacketID == 0 || snU16 > b.current.PacketID+16 {
+		b.flush()
+		b.current.PacketID = snU16
+		b.current.LostPackets = 0
+		return
+	}
+	b.current.LostPackets |= 1 << (snU16 - b.current.PacketID - 1)
+}
+
+func (b *nackPairBuilder) flush() {
+	if b.current.PacketID != 0 {
+		b.pairs = append(b.pairs, b.current)
+	}
+}
+
+func (b *nackPairBuilder) build() []rtcp.NackPair {
+	b.flush()
+	return b.pairs
 }
